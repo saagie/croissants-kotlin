bbda078fe63492abadd208a7b908379549e716de
Kevin Vézier <kevin@saagie.com>
2018-04-05 10:58:49 +0200

    clean project

--- src/main/kotlin/io/saagie/croissants/dao/PropositionDao.kt
+++
@@ -1,9 +0,0 @@
-package io.saagie.croissants.dao
-
-import io.saagie.croissants.domain.Proposition
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PropositionDao : MongoRepository<Proposition, String> {
-}
