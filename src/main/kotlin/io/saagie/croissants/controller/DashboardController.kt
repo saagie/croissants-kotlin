5e2d63fb3ea096d93a34405efd12383d8becc8b5
Kevin Vézier <kevin@saagie.com>
2018-04-07 09:57:40 +0200

    disable auth

--- src/main/kotlin/io/saagie/croissants/controller/DashboardController.kt
+++ src/main/kotlin/io/saagie/croissants/controller/DashboardController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DashboardController(val userService: UserService) {
 
-    @PreAuthorize("isAnonymous() or isFullyAuthenticated()")
     @GetMapping("/dashboard")
     fun dashboard(): Dashboard {
         return Dashboard(
