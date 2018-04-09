d7f175ffd2ae7c4dfe2ff1f3d0c874657e0f5c38
@guillaume.prevost <guillaume.prevost@saagie.com>
2018-04-09 11:22:56 +0200

    Add the draw function

---
+++ src/main/kotlin/io/saagie/croissants/service/DrawService.kt
@@ -0,0 +1,33 @@
+package io.saagie.croissants.service
+
+import io.saagie.croissants.domain.User
+import io.saagie.croissants.service.UserService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+class DrawService(val userService: UserService) {
+
+    @GetMapping("/test_draw")
+    fun drawUser(): User{
+        val userList = userService.findUsersToDraw()
+        var coef: Int = 0
+        var drawList: MutableList<User> = mutableListOf()
+        var rand: Int
+
+        userList.forEach {
+            coef = userService.getWeightedCoefficient(it)
+            println("coef : ${coef}")
+            for (i in 1..coef){
+                drawList.add(it)
+            }
+        }
+
+        drawList.shuffle()
+
+        rand = Random().nextInt(drawList.size)
+
+        return drawList[rand]
+    }
+}
