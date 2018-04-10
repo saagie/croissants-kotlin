4ac73a0828501a51994681b1f18c34f60a1e31a5
@guillaume.prevost <guillaume.prevost@saagie.com>
2018-04-10 12:05:58 +0200

    Use the sendDM function also selection in scheduleSelection

--- src/main/kotlin/io/saagie/croissants/service/DrawService.kt
+++ src/main/kotlin/io/saagie/croissants/service/DrawService.kt
@@ -4,7 +4,6 @@ import io.saagie.croissants.domain.User
 import io.saagie.croissants.slack.SlackBot
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.RestController
-import java.time.Instant
 import java.time.LocalDate
 import java.util.*
 
@@ -17,14 +16,16 @@ class DrawService(val userService: UserService,
     @Scheduled(cron = "0 0 8 * * WED")
     fun scheduleSelection() {
         val nextFriday = utilService.getNextFriday()
-        if (historyService.getByDate(utilService.localDatetoDate(nextFriday)) == null) {
-            slackBot.selection(this.drawUser(), nextFriday)
+        if (historyService.getByDate(utilService.localDateToDate(nextFriday)) == null) {
+            val userDraw = this.drawUser()
+            slackBot.sendDM(userDraw,announcementMessage(nextFriday))
+            slackBot.sendDM(userService.getByEmail("kevin@saagie.com"),announcementMessage(nextFriday,userDraw))
         }
     }
 
     @Scheduled(cron = "0 0 1 * * *")
     fun updateCoef() {
-        val history = historyService.getByDate(utilService.localDatetoDate(LocalDate.now()))
+        val history = historyService.getByDate(utilService.localDateToDate(LocalDate.now()))
         if (history != null) {
             var user = userService.getByEmail(history.emailUser as String)
             user.coefficient=1
@@ -51,5 +52,17 @@ class DrawService(val userService: UserService,
         rand = Random().nextInt(drawList.size)
 
         return drawList[rand]
+    }
+
+    fun announcementMessage(date: LocalDate): String {
+        var message = "You have been drawn to bring the croissants on ${date}"
+
+        return message
+    }
+
+    fun announcementMessage(date: LocalDate, user: User): String {
+        var message = "${user.username} have been drawn to bring the croissants on ${date}"
+
+        return message
     }
 }
